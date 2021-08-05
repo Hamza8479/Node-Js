ee695fcb9c85d14181fcf3b4d2a782285368cda8
Hamza <hamzazaman500@gmail.com>
2021-08-05 15:39:48 +0500

    Argument Parsing with Yargs

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,21 +1,62 @@
-const validator = require("validator");
-const getNotes = require("./notes");
 const chalk = require("chalk");
+const yargs = require("yargs");
+const getNotes = require("./notes");
 
-const getN = getNotes();
-console.log(getN);
+// create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: () => {
+    console.log("Adding a new note!");
+  },
+});
 
-const bl = chalk.green(validator.isEmail("hamza.zaman40@gmail.com"));
-const gr = chalk.yellow(validator.isURL("http:/ajkhda"));
+// create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a new note",
+  handler: () => {
+    console.log("Removing the note!");
+  },
+});
 
-console.log(bl);
-console.log(gr);
+// create list command
+yargs.command({
+  command: "list",
+  describe: "List your notes",
+  handler: () => {
+    console.log("Listing out all notes!");
+  },
+});
 
-console.log(chalk.bgRed("yellow"));
-console.log(chalk.bold("Hamza"));
+// create read command
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: () => {
+    console.log("Reading a note!");
+  },
+});
 
-console.log(chalk.cyan.underline.bold("Hamzaaaaa"));
-console.log(chalk.red.underline.bold("Hamzaaaaa"));
+console.log(yargs.argv);
+
+// const validator = require("validator");
+// const getN = getNotes();
+// console.log(getN);
+
+// const bl = chalk.green(validator.isEmail("hamza.zaman40@gmail.com"));
+// const gr = chalk.yellow(validator.isURL("http:/ajkhda"));
+
+// console.log(bl);
+// console.log(gr);
+
+// console.log(chalk.bgRed("yellow"));
+// console.log(chalk.bold("Hamza"));
+
+// console.log(chalk.cyan.underline.bold("Hamzaaaaa"));
+// console.log(chalk.red.underline.bold("Hamzaaaaa"));
+
+// console.log(process.argv[2]);
 
 // const add = require("./utils");
 
